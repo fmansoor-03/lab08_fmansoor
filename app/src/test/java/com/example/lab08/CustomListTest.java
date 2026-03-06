f5ce5eb0c7b38a275ca0abb72020dd3c931cfda5
fmansoor-03 <fawazarshad30@gmail.com>
2026-03-06 12:54:59 -0700

    fail test cases

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -8,8 +8,35 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
-        // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+        assertTrue(list.hasCity(edmonton));
+    }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        City vancouver = new City("Vancouver", "BC");
+
+        assertEquals(0, list.countCities());
+        list.addCity(calgary);
+        assertEquals(1, list.countCities());
+        list.addCity(edmonton);
+        list.addCity(vancouver);
+        assertEquals(3, list.countCities());
+        list.deleteCity(calgary);
+        assertEquals(2, list.countCities());
+    }
 }
